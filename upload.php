74aa412639afaaeb6adef2b5166a06157dcf9f30
cabri89 <cabridens@gmail.com>
2017-04-05 19:19:27 +0200

    add extension txt to uploaded file

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 if (isset($_FILES["htmlFile"])) {
     $uploaddir = 'files/';
 
-    $fileName = date('m-d-Y-H-i-s-').rand();
+    $fileName = date('m-d-Y-H-i-s-').rand().".txt";
     $uploadfile = $uploaddir . $fileName;
 
     if (move_uploaded_file($_FILES['htmlFile']['tmp_name'], $uploadfile)) {
